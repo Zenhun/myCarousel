7b30d92517d4b7b24e5741935a67f694477a7df5
Zenhun <sasa.zezelic@gmail.com>
2016-06-29 03:08:01 +0200

    first commit
    Add project files

---
+++ NoviCarousel/index.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="NoviCarousel.index" %>
+
+<!DOCTYPE html>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>My Own Little Carousel</title>
+    <link href="res/myCss.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="res/myJQ.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="wrapper">
+        <div id="carousel">
+            <div id="frame">
+                <div class="images">
+                    <ul>
+                        <li class="image active-image"><img src="res/images/tree.jpg" width="600" height="400"/></li>
+                        <li class="image"><img src="res/images/trail.jpg" width="600" height="400"/></li>
+                        <li class="image"><img src="res/images/train.jpg" width="600" height="400"/></li>
+                        <li class="image"><img src="res/images/tornado.jpg" width="600" height="400"/></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="controls">
+                <div class="left-arrow" title="Previous image"><</div>
+                <div class="dots">
+                    <ul class="dots">
+                        <li class="dot active-dot"></li>
+                        <li class="dot"></li>
+                        <li class="dot"></li>
+                        <li class="dot"></li>
+                    </ul>
+                </div>
+                <div class="right-arrow" title="Next image">></div>
+            </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ NoviCarousel/res/myJQ.js
@@ -0,0 +1,64 @@
+﻿$(document).ready(function () {
+    var currentSlide;
+    var nextSlide;
+    var index;
+    var move;
+
+    window.setTimeout(moveNext, 2000);
+
+    function moveSlide(currentSlide, nextSlide, move, index) {
+        $('.image').animate(move, 600);
+
+        currentSlide.removeClass('active-image');
+        nextSlide.addClass('active-image');
+
+        $("body").find('.dot').eq(index).addClass('active-dot').siblings('.dot').removeClass('active-dot');
+    };
+
+    var moveNext = function () {
+        currentSlide = $('.active-image');
+        nextSlide = currentSlide.next();
+        if (nextSlide.length === 0) {
+            nextSlide = $('.image').first();
+            move = { "left": "+=1800px" };
+            index = 0;
+        } else {
+            move = { "left": "-=600px" };
+            index = nextSlide.index();
+        }
+
+        moveSlide(currentSlide, nextSlide, move, index);
+    };
+
+
+    $('li.image, .right-arrow').click(moveNext);
+
+    $('.left-arrow').click(function () {
+        currentSlide = $('.active-image');
+        nextSlide = currentSlide.prev();
+        if (nextSlide.length === 0) {
+            nextSlide = $('.image').last();
+            move = { "left": "-=1800px" };
+            index = 3;
+        } else {
+            move = { "left": "+=600px" };
+            index = nextSlide.index();
+        }
+
+        moveSlide(currentSlide, nextSlide, move, index);
+    });
+
+    $('.dot').click(function () {
+        var firstDot = $('.active-dot').index();
+        var secondDot;
+        
+        $(this).addClass('active-dot').siblings('.dot').removeClass('active-dot');
+        secondDot = $(this).index();
+
+        var diff = (secondDot - firstDot) * 600;
+        move = { "left": "-=" + diff + "px" };
+        $('.image').animate(move, 600);
+
+        $("body").find('.image').eq(secondDot).addClass('active-image').siblings('.image').removeClass('active-image');
+    });     
+});
